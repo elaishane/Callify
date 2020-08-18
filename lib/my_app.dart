c4b1054920ab948211298e9085e5b43e0fcf8d56
Yash Adulkar <yash151099@gmail.com>
2020-08-18 17:28:31 +0530

    Added UI

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,3 +1,4 @@
+import 'package:apphigh_tech_assignment/swipe_button.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatelessWidget {
@@ -7,20 +8,47 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.blue,
-        body: Center(
-          child: Text(
-            "callify",
-            textAlign: TextAlign.center,
-            maxLines: 1,
-            style: Theme.of(context).textTheme.headline4.copyWith(
+        body: Stack(
+          children: [
+            Center(
+              child: Text(
+                "callify",
+                textAlign: TextAlign.center,
+                maxLines: 1,
+                style: Theme.of(context).textTheme.headline4.copyWith(
+                      color: Colors.white,
+                      fontSize: 35,
+                      fontFamily: "AvenirNextRounded",
+                      fontWeight: FontWeight.bold,
+                      fontStyle: FontStyle.normal,
+                      textBaseline: TextBaseline.ideographic,
+                      letterSpacing: 0.1,
+                    ),
+              ),
+            ),
+            Positioned(
+              bottom: 45.0,
+              left: 25.0,
+              right: 25.0,
+              child: SwipeButton(
+                thumb: Icon(
+                  Icons.arrow_forward_ios,
                   color: Colors.white,
-                  fontSize: 35,
-                  fontWeight: FontWeight.bold,
-                  fontStyle: FontStyle.normal,
-                  textBaseline: TextBaseline.ideographic,
-                  letterSpacing: 0.1,
                 ),
-          ),
+                content: Center(
+                  child: Text(
+                    "Swipe to Call",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                      fontFamily: "AvenirNextRounded",
+                    ),
+                  ),
+                ),
+                onChanged: (result) {},
+              ),
+            ),
+          ],
         ),
       ),
     );
