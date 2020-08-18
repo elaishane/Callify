1d9d19186e67858aabbb99801652007eaf808687
Yash Adulkar <yash151099@gmail.com>
2020-08-19 02:19:43 +0530

    App Configuration

---
+++ lib/config.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppConfig {
+  //App General Information
+  static const String appName = "callify";
+  static const Color appColor = Colors.blue;
+  static const String fontFamily = "AvenirNextRounded";
+}
