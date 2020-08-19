428b40c90a75a7eab30beb1831b6adf830cdc26c
Yash Adulkar <yash151099@gmail.com>
2020-08-19 14:38:27 +0530

    Configuration Updated

--- lib/config.dart
+++ lib/config.dart
@@ -5,4 +5,5 @@ class AppConfig {
   static const String appName = "callify";
   static const Color appColor = Colors.blue;
   static const String fontFamily = "AvenirNextRounded";
+  static const String agora_App_Id = 'c5d0de3117854a879bdea7d9a5f01a72';
 }
