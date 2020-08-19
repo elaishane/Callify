ac5ea79c5ac98c0c7e0486dfe445c558b9b248f4
Yash Adulkar <yash151099@gmail.com>
2020-08-20 03:16:13 +0530

    Completed

---
+++ lib/home_page.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        bottom: PreferredSize(
+          preferredSize: Size.fromHeight(130),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                "assets/logo.png",
+                height: 70,
+                width: 70,
+                filterQuality: FilterQuality.high,
+              ),
+//              Icon(
+//                Icons.video_call,
+//                color: Colors.white,
+//                size: 75.0,
+//              ),
+              Text(
+                "callify",
+                style: Theme.of(context).textTheme.headline5.copyWith(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: AppConfig.fontFamily,
+                      letterSpacing: 0.3,
+                    ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                "Enjoy free hd video calls",
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: CallButton(),
+      bottomNavigationBar: BottomAdSection(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,65 +1,21 @@
 import 'package:apphigh_tech_assignment/config.dart';
+import 'package:apphigh_tech_assignment/home_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
-import 'bottom_add_section.dart';
-import 'call_button.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: AppConfig.appName,
       home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        bottom: PreferredSize(
-          preferredSize: Size.fromHeight(130),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(
-                Icons.video_call,
-                color: Colors.white,
-                size: 75.0,
-              ),
-              Text(
-                "callify",
-                style: Theme.of(context).textTheme.headline5.copyWith(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: AppConfig.fontFamily,
-                      letterSpacing: 0.3,
-                    ),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              Text(
-                "Enjoy free hd video calls",
-                style: TextStyle(
-                  color: Colors.white,
-                ),
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-            ],
-          ),
-        ),
-      ),
-      body: CallButton(),
-      bottomNavigationBar: BottomAdSection(),
     );
   }
 }
